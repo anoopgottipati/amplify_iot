dc9da9a8c767d431129c5a6789b0f7d8a30dd67f
anoopgottipati <anoopgusgmail.com>
2024-12-30 16:35:46 -0500

    sign up

--- src/App.js
+++ src/App.js
@@ -8,6 +8,55 @@ import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(awsconfig);
 
+// Custom sign up fields
+const customSignUpFields = [
+  {
+    label: 'Enter your email',
+    key: 'email',
+    required: true,
+    displayOrder: 1,
+    type: 'email',
+  },
+  {
+    label: 'Enter your password',
+    key: 'password',
+    required: true,
+    displayOrder: 2,
+    type: 'password',
+  },
+  {
+    label: 'Confirm your password',
+    key: 'confirmPassword',
+    required: true,
+    displayOrder: 3,
+    type: 'password',
+  },
+  {
+    label: 'Preferred Username',
+    key: 'username',
+    required: true,
+    displayOrder: 4,
+    type: 'text',
+  },
+];
+
+function App({ signOut, user }) {
+  return (
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
+  );
+}
+
+// Wrap your App component with withAuthenticator and pass custom sign up fields
+export default withAuthenticator(App, {
+  signUpFields: customSignUpFields
+});
+
+/*
+
+
 function App({ signOut, user }) {
   return (
     <>
@@ -18,3 +67,5 @@ function App({ signOut, user }) {
 }
 
 export default withAuthenticator(App);
+
+*/
