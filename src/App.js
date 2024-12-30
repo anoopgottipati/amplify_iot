8b2f90b3a64ebb2edbb560c627ea18683d3b3dd3
anoopgottipati <anoopgusgmail.com>
2024-12-30 16:57:27 -0500

    sign up

--- src/App.js
+++ src/App.js
@@ -1,71 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Amplify, Auth } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import awsconfig from './aws-config';
 import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(awsconfig);
 
-// Custom sign up fields
-const customSignUpFields = [
-  {
-    label: 'Enter your email',
-    key: 'email',
-    required: true,
-    displayOrder: 1,
-    type: 'email',
-  },
-  {
-    label: 'Enter your password',
-    key: 'password',
-    required: true,
-    displayOrder: 2,
-    type: 'password',
-  },
-  {
-    label: 'Confirm your password',
-    key: 'confirmPassword',
-    required: true,
-    displayOrder: 3,
-    type: 'password',
-  },
-  {
-    label: 'Preferred Username',
-    key: 'username',
-    required: true,
-    displayOrder: 4,
-    type: 'text',
-  },
-];
-
+// Main App component
 function App({ signOut, user }) {
   return (
     <>
-      <h1>Hello {user.username}</h1>
+      <h1>Hello, {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
     </>
   );
 }
 
-// Wrap your App component with withAuthenticator and pass custom sign up fields
-export default withAuthenticator(App, {
-  signUpFields: customSignUpFields
-});
-
-/*
-
-
-function App({ signOut, user }) {
-  return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
-  );
-}
-
+// Wrap your App component with withAuthenticator (using the default sign-up fields)
 export default withAuthenticator(App);
-
-*/
