ae354f8457c5de2e23e3eaba1bf833479f348ede
anoopgottipati <anoopgusgmail.com>
2025-01-02 14:21:22 -0500

    device list

--- src/Devices.js
+++ src/Devices.js
@@ -7,7 +7,7 @@ const Devices = () => {
 
     useEffect(() => {
         // Fetch all devices
-        fetch('https://x1jv42435a.execute-api.us-east-1.amazonaws.com/prod/devices') // Replace with domain API endpoint
+        fetch('https://cn6ymcsbk8.execute-api.us-east-1.amazonaws.com/prod/devices') // Replace with domain API endpoint
             .then(response => response.json())
             .then(data => setDevices(data))
             .catch(error => console.error('Error fetching devices:', error));
