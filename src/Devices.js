c91966acfd618b1f84f54eefb902ff2343ec3e9b
anoopgottipati <anoopgusgmail.com>
2025-01-02 20:03:52 -0500

    device list

--- src/Devices.js
+++ src/Devices.js
@@ -7,7 +7,7 @@ const Devices = () => {
 
     useEffect(() => {
         // Fetch all devices
-        fetch('https://6htty5n9ai.execute-api.us-east-1.amazonaws.com/prod/devices', {
+        fetch('https://y300xh4dwi.execute-api.us-east-1.amazonaws.com/prod/devices', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
